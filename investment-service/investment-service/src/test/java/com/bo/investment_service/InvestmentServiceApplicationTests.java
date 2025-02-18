bb6df61ba6b1a180b4c0101820b4cf701f0469b4
Eduardo Arruda Remião <66395784+duduremiao@users.noreply.github.com>
2025-02-18 14:48:54 -0300

    spring microsservices creation

---
+++ investment-service/investment-service/src/test/java/com/bo/investment_service/InvestmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bo.investment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification-service/notification-service/src/test/java/com/bo/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bo.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
